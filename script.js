0b01224841fbfe6ebbb4a216d4708ff2f41e2b09
Kaleb Moore <k4l3bmoore@gmail.com>
2022-05-25 16:47:18 -0600

    Create Var for all input fields

---
+++ script.js
@@ -0,0 +1,9 @@
+//get all the input fields and their values
+
+let fistName = document.getElementById("first-name").value;
+let lastName = document.getElementById("last-name").value;
+let email = document.getElementById("email").value;
+let phoneNumber = document.getElementById("phone-number").value;
+let password = document.getElementById("password").value;
+let confirmPassword = document.getElementById("confirm password").value;
+
