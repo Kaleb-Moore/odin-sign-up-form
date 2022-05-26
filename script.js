116beefdfaffd393b2d81e0fe159e15ae11dc6be
Kaleb Moore <k4l3bmoore@gmail.com>
2022-05-25 21:10:44 -0600

    Create Javascript Validation for firstName

--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 //get all the input fields and their values
 
-let fistName = document.getElementById("first-name").value;
-let lastName = document.getElementById("last-name").value;
-let email = document.getElementById("email").value;
-let phoneNumber = document.getElementById("phone-number").value;
-let password = document.getElementById("password").value;
-let confirmPassword = document.getElementById("confirm password").value;
+let firstName = document.getElementById("first-name");
+let lastName = document.getElementById("last-name");
+let email = document.getElementById("email");
+let phoneNumber = document.getElementById("phone-number");
+let password = document.getElementById("password");
+let confirmPassword = document.getElementById("confirm password");
 
+let nameError = document.createElement('p');
+let nameText = document.createTextNode("* Name can not have numbers or symbols");
+
+const onlyLetters = new RegExp("^[a-zA-z]+$");
+
+firstName.addEventListener("keyup", (e) => {
+    
+    console.log(e.target.value)
+    console.log(onlyLetters.test(e.target.value))
+    if (onlyLetters.test(e.target.value)){
+        firstName.classList.add("valid");
+        firstName.classList.remove("invalid");
+        nameError.remove();
+    } else if (e.target.value == "") {
+        firstName.classList.remove("valid");
+        firstName.classList.remove("invalid");
+        nameError.remove();
+    } else if(!(onlyLetters.test(e.target.value))) {
+        firstName.classList.remove("valid");
+        firstName.classList.add("invalid");
+        nameError.appendChild(nameText);
+        nameError.classList.add("error-text");
+        firstName.parentElement.appendChild(nameError);
+
+    } 
+})
