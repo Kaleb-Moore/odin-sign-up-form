b82634cd5089562efad514bf513a16d3e45530f2
Kaleb Moore <k4l3bmoore@gmail.com>
2022-05-26 12:56:33 -0600

    Create Javascript Validation for LastName, Email and Phone

--- script.js
+++ script.js
@@ -9,13 +9,14 @@ let confirmPassword = document.getElementById("confirm password");
 
 let nameError = document.createElement('p');
 let nameText = document.createTextNode("* Name can not have numbers or symbols");
+let emailText = document.createTextNode("* Please enter a valid email address");
+let phoneNumberText = document.createTextNode("* Please enter a valid phone number");
 
-const onlyLetters = new RegExp("^[a-zA-z]+$");
+const onlyLetters = new RegExp("^[a-zA-z-']+$");
+const emailReg = new RegExp("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$");
+const phoneNumberReg = new RegExp("^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$");
 
 firstName.addEventListener("keyup", (e) => {
-    
-    console.log(e.target.value)
-    console.log(onlyLetters.test(e.target.value))
     if (onlyLetters.test(e.target.value)){
         firstName.classList.add("valid");
         firstName.classList.remove("invalid");
@@ -30,6 +31,60 @@ firstName.addEventListener("keyup", (e) => {
         nameError.appendChild(nameText);
         nameError.classList.add("error-text");
         firstName.parentElement.appendChild(nameError);
-
     } 
-})
+});
+
+lastName.addEventListener("keyup", (e) => {
+    if (onlyLetters.test(e.target.value)){
+        lastName.classList.add("valid");
+        lastName.classList.remove("invalid");
+        nameError.remove();
+    } else if (e.target.value == "") {
+        lastName.classList.remove("valid");
+        lastName.classList.remove("invalid");
+        nameError.remove();
+    } else if(!(onlyLetters.test(e.target.value))) {
+        lastName.classList.remove("valid");
+        lastName.classList.add("invalid");
+        nameError.appendChild(nameText);
+        nameError.classList.add("error-text");
+        lastName.parentElement.appendChild(nameError);
+    } 
+});
+
+email.addEventListener("focusout", (e) => {
+    if (emailReg.test(e.target.value)){
+        email.classList.add("valid");
+        email.classList.remove("invalid");
+        nameError.remove();
+    } else if (e.target.value == "") {
+        email.classList.remove("valid");
+        email.classList.remove("invalid");
+        nameError.remove();
+    } else if(!(emailReg.test(e.target.value))) {
+        email.classList.remove("valid");
+        email.classList.add("invalid");
+        nameError.appendChild(emailText);
+        nameError.classList.add("error-text");
+        email.parentElement.appendChild(nameError);
+    } 
+});
+
+phoneNumber.addEventListener("focusout", (e) => {
+    console.log('test');
+    if (phoneNumberReg.test(e.target.value)){
+        phoneNumber.classList.add("valid");
+        phoneNumber.classList.remove("invalid");
+        nameError.remove();
+    } else if (e.target.value == "") {
+        phoneNumber.classList.remove("valid");
+        phoneNumber.classList.remove("invalid");
+        nameError.remove();
+    } else if(!(phoneNumberReg.test(e.target.value))) {
+        phoneNumber.classList.remove("valid");
+        phoneNumber.classList.add("invalid");
+        nameError.appendChild(phoneNumberText);
+        nameError.classList.add("error-text");
+        phoneNumber.parentElement.appendChild(nameError);
+    } 
+});
